5f67a81ce97889c42e9e5a378ec011348a2fdc52
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-12-19 18:09:05 -0500

    BUILD: Pushed to heroku

---
+++ public/precache-manifest.597c6ff3ce47b2ab9d3a0e4ad032492c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cebee7067eb3f065acbcc0199ee402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd36cd0dbdc906f0614d",
+    "url": "/static/css/main.0af08e08.chunk.css"
+  },
+  {
+    "revision": "510785a61da06d34fc45",
+    "url": "/static/js/2.1de5fe9b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.1de5fe9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd36cd0dbdc906f0614d",
+    "url": "/static/js/main.3bed8d33.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
