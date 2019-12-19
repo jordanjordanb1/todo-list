353cc1aa5cdc58ebc810688e2c7be48a2a13969b
Jordan Barbosa <jordansbarbosa01@gmail.com>
2019-12-18 23:54:32 -0500

    Removed unneeded actiontypes

--- client/src/redux/ActionTypes.ts
+++ client/src/redux/ActionTypes.ts
@@ -1,5 +1,1 @@
 export const LOAD_TODO_ITEMS: string = "LOAD_TODO_ITEMS";
-export const ADD_TODO_ITEM: string = "ADD_TODO_ITEM";
-export const DELETE_TODO_ITEM: string = "DELETE_TODO_ITEM";
-export const EDIT_TODO_ITEM: string = "EDIT_TODO_ITEM";
-export const MARK_ITEM_DONE: string = "MARK_ITEM_DONE";
--- client/src/redux/reducers/todo.ts
+++ client/src/redux/reducers/todo.ts
@@ -9,14 +9,6 @@ export const todo = (
     switch (action.type) {
         case ActionTypes.LOAD_TODO_ITEMS:
             return { ...state, todo_items: action.payload };
-        case ActionTypes.ADD_TODO_ITEM:
-            return { ...state, todo_items: action.payload };
-        case ActionTypes.DELETE_TODO_ITEM:
-            return { ...state, todo_items: action.payload };
-        case ActionTypes.EDIT_TODO_ITEM:
-            return { ...state, todo_items: action.payload };
-        case ActionTypes.MARK_ITEM_DONE:
-            return { ...state, todo_items: action.payload };
         default:
             return state;
     }
